58f5b84ac7127ea7268194bc30a23562ce908e87
meta-ela <anxhelameta@live.it>
2022-06-09 16:33:48 +0200

    Create repo and files

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const appVue = new Vue({
+    // selezioni l'id in html dove verrà scritto tutto il codice Vue
+    el: "#app",
+
+    // dati per Vue
+    data: {
+        // chiavi
+    },
+
+    // funzioni
+    methods: {
+        //
+    },
+});
